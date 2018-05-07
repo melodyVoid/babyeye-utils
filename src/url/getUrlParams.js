b80c364596ce3ae84a25fc7309b1e6c77384b520
melodyVoid <botaoxyz@gmail.com>
2018-05-07 14:31:07 +0800

    fix:修改 getUrlParams 函数，增加健壮性

--- src/url/getUrlParams.js
+++ src/url/getUrlParams.js
@@ -1,7 +1,10 @@
 export default function getUrlParams(url) {
-  return url.match(/([^?=&]+)(=([^&]*))/g).reduce((obj, item) => {
-    const [key, value] = item.split('=')
-    obj[key] = value
-    return obj
-  }, {})
+  const paramsArr = url.match(/([^?=&]+)(=([^&]*))/g)
+  return paramsArr
+    ? paramsArr.reduce((obj, item) => {
+      const [key, value] = item.split('=')
+      obj[key] = value
+      return obj
+    }, {})
+    : null
 }
