bef01728d830220fba262a9c28a38421230ba3da
melodyVoid <botaoxyz@gmail.com>
2018-06-12 10:54:43 +0800

    fix:测试scrollTo

--- demo/main.js
+++ demo/main.js
@@ -3,20 +3,20 @@ import bUtils from '../src/index'
 
 console.log(bUtils)
 
-// const oDiv = document.createElement('div')
+const oDiv = document.createElement('div')
 
-// oDiv.style.height = '200px'
-// oDiv.style.width = '200px'
-// oDiv.style.position = 'absolute'
-// oDiv.style.top = '1000px'
-// oDiv.style.background = 'red'
-// document.body.appendChild(oDiv)
-// document.body.style.height = '2000px'
-// console.dir(oDiv)
+oDiv.style.height = '200px'
+oDiv.style.width = '200px'
+oDiv.style.position = 'absolute'
+oDiv.style.top = '1000px'
+oDiv.style.background = 'red'
+document.body.appendChild(oDiv)
+document.body.style.height = '2000px'
+console.dir(oDiv)
 
-// document.onclick = () => {
-//   bUtils.scrollTo(oDiv, 8)
-// }
+document.onclick = () => {
+  bUtils.scrollTo(oDiv)
+}
 
 // const obj = {
 //   name: 'jack',
